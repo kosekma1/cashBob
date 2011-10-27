5af2412cc488452d61c36d1b8079a0c873216252
kosekm <kosekm@VI070.dom01.ad>
2011-10-27 13:17:24 +0200

    Test Commit - remove print text.

--- RestauraceFelWorkShift/src/main/java/cz/cvut/fel/restauracefel/restauracefelworkshift/App.java
+++ RestauraceFelWorkShift/src/main/java/cz/cvut/fel/restauracefel/restauracefelworkshift/App.java
@@ -8,12 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("Added text");
-        System.out.println("New text");
-        System.out.println("New text 3");
-        System.out.println("New text 4");
-        System.out.println("New text 5");
-        System.out.println("New text 6");
+        System.out.println( "Hello World!" );   
     }
 }
